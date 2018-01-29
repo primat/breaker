916467e3d0c129e6e6701e02b801f305f9fc42a6
Mat Price <mathieu.price@bellmedia.ca>
2018-01-29 09:26:20 -0500

    fiddling with demos

--- demo/app.js
+++ demo/app.js
@@ -4,7 +4,7 @@ app.config(['breakpointProvider', function (breakpointProvider) {
 
     breakpointProvider.debounce = 100;
     breakpointProvider.initBind = true;
-    //breakpointProvider.initBreakpoints = [500, 800, 1000];
+    breakpointProvider.initBreakpoints = [500, 800, 1000];
 }]);
 
 
@@ -14,10 +14,7 @@ app.directive('breakpointTest', [function () {
         scope: true,
         controller: ['$scope', 'breakpoint', function ($scope, breakpoint) {
 
-            var self = this;
-
-            // Register the breakpoint event for this scope
-            breakpoint.addBreakpoints([500, 800, 1000], $scope);
+            //breakpoint.addBreakpoints([500, 800, 1000], $scope);
 
             // Listen for breakpoint events
             $scope.$on('breakpoint', function (data, $event) {
@@ -37,9 +34,8 @@ app.directive('breakpointTest2', [function () {
         controllerAs: 'ctrl',
         bindToController: true,
         template: '<div>\n' +
-        '            <div>\n' +
-        '                Viewport width: {{ point }}\n' +
-        '            </div>\n' +
+        '             Breakpoints on $rootScope + isolate scope: [600, 700, 900]<br />' +
+        '             Viewport width: {{ point }}\n' +
         '          </div>',
         controller: ['$scope', 'breakpoint', function ($scope, breakpoint) {
 
@@ -48,10 +44,11 @@ app.directive('breakpointTest2', [function () {
             // Register the breakpoint event for this scope
             breakpoint.addBreakpoints([600, 700, 900], $scope);
 
-
             // Listen for breakpoint events
             $scope.$on('breakpoint', function (data, $event) {
-                $scope.point = $event.point;
+                if (data.targetScope.$parent !== null) {
+                    $scope.point = $event.point;
+                }
             });
 
         }]
